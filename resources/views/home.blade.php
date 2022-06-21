092bb41221d555af592155a821fb000679488132
teenouka <teenwcrp789@gmail.com>
2022-06-21 09:55:07 +0700

    test teen first push

---
+++ resources/views/home.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=(), initial-scale=1.0">
+    <title>Test</title>
+    <!-- Style -->
+    <link href="{{asset('css/app.css')}}" rel="stylesheet">
+</head>
+<body>
+    <!--React root DOM -->
+    <div id="root"></div>
+
+    <!-- React JS -->
+<script src="{{asset('js/app.js')}}" defer></script>
+</body>
+</html>
